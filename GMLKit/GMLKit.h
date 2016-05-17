fc34e30983a926645a3de0339c007f0b8276a289
guominglong <guominglong@guominglongdemacbook-pro.local>
2016-05-17 13:50:26 +0800

    Initial Commit

---
+++ GMLKit/GMLKit.h
@@ -0,0 +1,19 @@
+//
+//  GMLKit.h
+//  GMLKit
+//
+//  Created by guominglong on 16/5/17.
+//  Copyright © 2016年 guominglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMLKit.
+FOUNDATION_EXPORT double GMLKitVersionNumber;
+
+//! Project version string for GMLKit.
+FOUNDATION_EXPORT const unsigned char GMLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMLKit/PublicHeader.h>
+
+
